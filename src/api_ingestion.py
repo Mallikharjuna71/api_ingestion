e7bc0e97cc1f89e93c2c55d848ab5e5f0c9f57ea
Mallikharjuna71 <meka.mallikharjunareddy@diggibyte.com>
2025-02-14 19:05:34 +0530

    API ingestion with ecommerece domain

--- src/api_ingestion.py
+++ src/api_ingestion.py
@@ -3,25 +3,24 @@ import requests
 url = 'https://api.restful-api.dev/objects'
 
 headers = {
-    'Authorization': 'Bearer <your_access_token>',  # Bearer token for authentication
-    'Content-Type': 'application/json',  # The body content is JSON
-    'Accept': 'application/json',  # The expected response format
-    'User-Agent': 'MyApp/1.0',  # Identifying the client
-    'X-API-Key': '<your_api_key>',  # Custom API Key authentication (if needed)
-    'Accept-Encoding': 'gzip, deflate',  # Supports compressed responses
-    'Cache-Control': 'no-cache',  # Ensuring the response is not cached
+    'Authorization': 'Bearer <your_access_token>', 
+    'Content-Type': 'application/json',  
+    'Accept': 'application/json', 
+    'User-Agent': 'MyApp/1.0',  
+    'X-API-Key': '<your_api_key>',  
+    'Accept-Encoding': 'gzip, deflate',  
+    'Cache-Control': 'no-cache',  
 }
 
 def get_data(url, headers):
     try:
         response = requests.get(url, headers)
-        # Check if the request was successful (status code 200)
         if response.status_code == 200:
-            data = response.json()  # Parse the JSON data from the response
-            print(data)  # Print or use the data
+            data = response.json()  
+            print(data)
         else:
             print(f"Request failed with status code {response.status_code}")
-    except requests.exceptions.RequestException as e:
+    except Exception as e:
         print(f"An error occurred: {e}")
 
 
@@ -34,7 +33,7 @@ def post_data(url, headers, body):
 # Handle the response
     if response.status_code == 200:
         print("User created successfully.")
-        print("Response Data:", response.json())  # Print the server's response data (if applicable)
+        print("Response Data:", response.json())  
     elif response.status_code == 400:
         print("Bad request. Check the data sent.")
         print("Response:", response.text)
